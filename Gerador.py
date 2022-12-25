e89fe187548aab80b9e7b4cdb33a88a105edf601
Gustavo Michels de Camargo <guga.power@hotmail.com>
2022-12-25 11:52:23 -0300

    Refatoração da função gerar

--- Gerador.py
+++ Gerador.py
@@ -3,21 +3,10 @@ import random
 class Gerador:
     def __init__(self, numeros):
         self.numeros = numeros
-
+    
     def gerar(self):
-        result = []
-
-        while len(result) < self.numeros:
-            n = random.randint(1, 60)
-
-            if n not in result:
-                result.append(n)
-            
-        return sorted(result)
+        return sorted([random.randint(1, 60) for _ in range(self.numeros)])
                 
-
-
-
 
 if __name__ == "__main__":
     numeros = int(input("Digite quantos numeros quer gerar: "))
